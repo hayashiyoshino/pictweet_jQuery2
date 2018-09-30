ecef936e96c0c005d73c55f8903246976d517d93
林佳志乃 <twinklestaryoshino@gmail.com>
2018-09-30 13:21:31 +0900

    definition comment#create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,14 @@
 class CommentsController < ApplicationController
   def create
-    # binding.pry
     @comment = Comment.create(text: comment_params[:text], tweet_id: comment_params[:tweet_id], user_id: current_user.id)
-    redirect_to "/tweets/#{@comment.tweet.id}"
+
+    respond_to do |format|
+      format.html { redirect_to tweet_path(params[:tweet_id])}
+      format.json
+    end
+    # respond_to do を使用し、リクエストされたformatによって処理を分けるようにする
+    # todoリストのアプリではformat.jsonに引数としてインスタンスを渡したが、今回はjbuilderを使ってJavaScriptに返すデータを作成する。
+    # jbuilderは、railsにデフォルトで入っているgemで、入力データをJSON形式で出力するテンプレートエンジン。
   end
 
   private
